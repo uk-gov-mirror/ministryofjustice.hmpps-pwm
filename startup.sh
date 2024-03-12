f570a9b94b7b153ea8f559f4a523901b6bbe0919
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-12 14:24:03 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,9 +5,9 @@ echo "${CONFIG_XML_BASE64}" | base64 -d > PwmConfiguration.xml.tpl
 
 export SES_PASSWORD=$(echo $SES_JSON | jq -r '.ses_smtp_password')
 export SES_USERNAME=$(echo $SES_JSON | jq -r '.ses_smtp_user')
+export CONFIG_PASSWORD_HASH=$(htpasswd -bnBC 10 "" "${CONFIG_PASSWORD}" | tr -d ':\n')
 
-CONFIG_PASSWORD_HASH=$(htpasswd -bnBC 10 "" "${CONFIG_PASSWORD}" | tr -d ':\n') \
-  envsubst <PwmConfiguration.xml.tpl >"${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
+envsubst <PwmConfiguration.xml.tpl >"${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
 
 unset SES_PASSWORD
 unset SES_USERNAME
