0d4ae2efb78c7d1c83dbcb354fccd47ca4cb9556
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-12 13:53:46 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,14 +2,15 @@
 
 echo 'Generating configuration...'
 echo "${CONFIG_XML_BASE64}" | base64 -d > PwmConfiguration.xml.tpl
+
+export SES_PASSWORD=$(echo $SES_JSON | jq -r '.ses_smtp_password')
+export SES_USERNAME=$(echo $SES_JSON | jq -r '.ses_smtp_user')
+
 CONFIG_PASSWORD_HASH=$(htpasswd -bnBC 10 "" "${CONFIG_PASSWORD}" | tr -d ':\n') \
   envsubst <PwmConfiguration.xml.tpl >"${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
 
-SES_PASSWORD=$(echo $SES_JSON | jq -r '.ses_smtp_password')
-SES_USERNAME=$(echo $SES_JSON | jq -r '.ses_smtp_user')
+unset SES_PASSWORD
+unset SES_USERNAME
 
-envsubst '$LDAP_PASSWORD' < "${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
-envsubst '$SES_PASSWORD' < "${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
-envsubst '$SES_USERNAME' < "${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
 echo 'Starting PWM...'
 catalina.sh run
