645931bec6167cbb7319f5869585ad61b1353c21
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-12 09:51:29 +0000

    Add SES support

--- startup.sh
+++ startup.sh
@@ -5,5 +5,7 @@ echo "${CONFIG_XML_BASE64}" | base64 -d > PwmConfiguration.xml.tpl
 CONFIG_PASSWORD_HASH=$(htpasswd -bnBC 10 "" "${CONFIG_PASSWORD}" | tr -d ':\n') \
   envsubst <PwmConfiguration.xml.tpl >"${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
 envsubst '$LDAP_PASSWORD' < "${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
+envsubst '$SES_PASSWORD' < "${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
+envsubst '$SES_USERNAME' < "${PWM_APPLICATIONPATH}/PwmConfiguration.xml"
 echo 'Starting PWM...'
 catalina.sh run
